3c6be53097b4fc31b1eb67e5e8a5aef1ba235f74
Justin Leone <jfleone97@gmail.com>
2019-02-10 12:05:41 -0800

    .

---
+++ ErrorCheckStudentInfo/ErrorCheckStudentInfo/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ErrorCheckStudentInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void validateDataButton_Click(object sender, EventArgs e)
+        {
+            string name, classID, itemDescrip, errorMsg;
+            double itemScore;
+            int semesterStanding = 1;
+
+            StreamReader inputFile;
+            if (File.Exists("StudentsInfo.txt"))
+            {
+                //StudentChecker sc = new StudentChecker();
+                StudentChecker sc = new StudentChecker(0, 120);
+                inputFile = File.OpenText("StudentsInfo.txt");
+                while (!inputFile.EndOfStream)
+                {
+                    //Input
+                    name = inputFile.ReadLine();
+                    classID = inputFile.ReadLine();
+                    itemDescrip = inputFile.ReadLine();
+                    semesterStanding = int.Parse(inputFile.ReadLine());
+                    itemScore = double.Parse(inputFile.ReadLine());
+
+                    //Error Checking
+                    if (!sc.CheckSemesterStanding(semesterStanding)
+                    {
+                        errorMsg = String.Format("Error: semesterStanding --> {0} - {1} - {2} - {3} - {4}",
+                            name, classID, itemDescrip, semesterStanding, itemScore);
+                        msgListBox.Items.Add(errorMsg);
+                    }
+                    if (!sc.CheckScore(itemScore))
+                    {
+                        // The {0} are argument indices for items
+                        errorMsg = String.Format("Error: ItemScore --> {0} - {1} - {2} - {3} - {4}",
+                            name, classID, itemDescrip, semesterStanding, itemScore);
+                        msgListBox.Items.Add(errorMsg);
+                    }
+                }
+                inputFile.Close();
+            }
+            else
+            {
+                MessageBox.Show("Error. File does not exist");
+            }
+        }
+    }
+}
---
+++ ErrorCheckStudentInfo/ErrorCheckStudentInfo/StudentChecker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorCheckStudentInfo
+{
+    class StudentChecker
+    {
+        private double _minScore;
+        private double _maxScore; //build accessors next class
+        private double _minSemesterStanding;
+        private double _maxSemesterStanding;
+        public StudentChecker()
+        {
+            _minScore = 0.0;
+            _maxScore = 100.0;
+            _minSemesterStanding = 1;
+            _maxSemesterStanding = 14;
+        }
+        //alternative to take in args for student checker
+        public StudentChecker(double minScore, double maxScore)
+        {
+            _minScore = minScore;
+            _maxScore = maxScore;
+            _minSemesterStanding = 1;
+            _maxSemesterStanding = 14;
+        }
+        public StudentChecker(double minScore, double maxScore, int minSemester, int maxSemester)
+        {
+            _minScore = minScore;
+            _maxScore = maxScore;
+            _minSemesterStanding = minSemester;
+            _maxSemesterStanding = maxSemester;
+        }
+
+        /// <summary>
+        /// Validates a students semester standing
+        /// </summary>
+        /// <param name="semester">Semester standing to be checked</param>
+        /// <returns>Returns if the semester is valid</returns>
+        public bool CheckSemesterStanding (int semester)
+        {
+            return !(semester < _minSemesterStanding || semester > _maxSemesterStanding);
+        }
+
+        /// <summary>
+        /// Validates a score against the minimum and maximum possible
+        /// </summary>
+        /// <param name="testScore">Score to be checked</param>
+        /// <returns>Return if the score is valid</returns>
+        public bool CheckScore(double testScore)
+        {
+            return testScore < _minScore || testScore > _maxScore ? false : true;
+        }
+    }
+}
