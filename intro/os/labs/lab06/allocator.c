de67c09b2b2a3e5ecab98133f57580fc346430cd
Justin <jfleone97@gmail.com>
2019-02-17 21:21:54 -0500

    it continues

--- intro/os/labs/lab06/allocator.c
+++ intro/os/labs/lab06/allocator.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 
-void alloc();
-void free(); //optional
-void view(); //view choose
+void initHeap(int size);
+void mallocator(int req);
+//void free(); //optional
+void view(); //view choice
 int choose(); //handles user choice
 
 
@@ -19,29 +21,34 @@ typedef struct __header_t {
     int magic; //unique identifier; NOT USED IN THIS ASSIGNMENT 
 } header_t;
 
-//struct node_t head = NULL;
+struct node_t *nodeHead = NULL;
+struct header_t *head = NULL;
 
 int main(int argc, char* argv[]) {
 
-    int h = atoi(argv[1]);
+    int heapSize = atoi(argv[1]);
+    int heap = 0; //what is this?
+    //initHeap(heapSize);
+
+    printf("%s %i %s %i", "\nheap: ", heap, ", 0, size=", heapSize);
     
     int choice = choose();
 
     switch (choice) {
 
-        case 1: //malloc
-            alloc();
+        case 1: //mallocator
+            mallocator(sizeof(node_t));
 	        break;
 
         case 2: //free optional
-            free();
+            //free();
             break;
 
         case 3: //coalesce optional
             break;
 
         case 4: //view
-            view();
+            //view();
             break;
 
         case 5: //quit
@@ -59,29 +66,54 @@ int main(int argc, char* argv[]) {
     return 0;
 }
  
-void alloc() {
+/*
+void initHeap(int size) {
+    //create an empty heap with 4KB of size
+    nodeHead = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+    nodeHead->size = 4096 - sizeof(node_t);
+    nodeHead->next = NULL;
+}*/
+
+
+void mallocator(int req) {
     // allocate memory from within the heap for N bytes
 
     printf("%s", "ENTER THE SIZE IN BYTES: ");
     int size = 0;
     scanf("%d", &size);
 
-    //struct node_t current = head;
-    struct node_t current = NULL;
-    current = (struct node_t*)malloc(sizeof(struct node_t));
+    struct node_t *current = nodeHead;
 
+    while (current->size < req+4) {
+        current = current->next;
+        if (current==NULL) {
+            printf("%s", "Error");
+            break;
+        }
+    }
+    struct node_t *old = NULL;
+    old->next = current->next;
+    old->size = current->size;
+
+    /*
+    new header_t = {size = req} -> &current;
+
+    current += req+4; //move pointer
+
+    new node_t = {size = old->size - req - 4, next = old->next} -> &current;
+    */
 }
 
-void free() {
-}
+//void free() {}
 
+/*
 void view() {
 
 
     printf("%s", "Traversing linked list of free regions...\n\n");
     //struct node_t current = head;
     struct node_t current = NULL;
-    current = (struct node_t*)malloc(sizeof(struct node_t));
+    current = (struct node_t*)mallocator(sizeof(struct node_t));
     while (current != NULL) {
 
         //calculate overhead info
@@ -89,11 +121,11 @@ void view() {
 
         current = current->next;
     }
-}
+}*/
 
 int choose(){
 
-    printf("%s", "\nALLOCATOR: ENTER AN OPTION\n  1. malloc\n  2. free\n  3. coalesce\n  4. view\n  5. quit\nCHOICE: ");
+    printf("%s", "\nALLOCATOR: ENTER AN OPTION\n  1. mallocator\n  2. free\n  3. coalesce\n  4. view\n  5. quit\nCHOICE: ");
     int c;
     scanf("%d", &c);
     return c;
