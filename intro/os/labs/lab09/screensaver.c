7d3e9033fbb2ff2276d9c20c24eb30217024b112
Justin <jfleone97@gmail.com>
2019-03-17 21:03:22 -0400

    finished lab 9 for real!

--- intro/os/labs/lab09/screensaver.c
+++ intro/os/labs/lab09/screensaver.c
@@ -5,12 +5,13 @@
 #define BUF_SIZE 100
 
 char buf[BUF_SIZE];
+
 void *printStuff()
-{
-	int bufSize = sizeof(buf)/sizeof(char);
-	
-	while (1){
-		usleep(2000000);
+{	
+	while (1)
+	{
+		//print the things, then wait before printing again
+		printf("%c", '\n');
 		for(int hiddenCount = 0; buf[hiddenCount]; hiddenCount++)
 		{
 			for(int wordCount = 0; buf[wordCount]; wordCount++)
@@ -20,7 +21,7 @@ void *printStuff()
 			}
 			printf("%c", '\n');
 		}
-		printf("%c", '\n');
+		usleep(2000000);
 	}
 }
 
@@ -28,19 +29,25 @@ void *processInput()
 {
 	gets(buf);
 }
+
 int main(int argc, char* argv[])
 {
-	//have the main thread process input and the spawned thread process output
-	gets(buf);
+	//spawn one thread to do input and one thread for output
 	pthread_t printThread;
 	pthread_t inputThread;
 
 	while (1)
 	{
+		//spawn the input thread first since we need input
 		pthread_create(&inputThread, NULL, processInput, NULL);
+		
+		//wait for the input thread to complete before doing anything else
+		//if an output thread is running it will continue to run alongside the input thread
 		pthread_join(inputThread, NULL);
+
+		//cancel any existing output threads before making a new one
+		pthread_cancel(printThread);
 		pthread_create(&printThread, NULL, printStuff, NULL);
-		//pthread_join(printThread, NULL);
 	}
 	return 0;
 }
